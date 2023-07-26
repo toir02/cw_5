26b5f041fd1c16bd6efbfca75ec160f8d38b74ed
toir02 <wend8765@gmail.com>
2023-07-26 12:21:18 +0300

    added DBManager class

--- src/DBManager.py
+++ src/DBManager.py
@@ -1,16 +1,61 @@
 import psycopg2
+from config import config
 
 
 class DBManager:
-    def __init__(self, database_name, params):
+    def __init__(self, database_name, params=config()):
         self.database_name = database_name
         self.params = params
 
     def get_companies_and_vacancies_count(self):
+
         try:
             connection = psycopg2.connect(database=self.database_name, **self.params)
+            with connection.cursor() as cursor:
+                cursor.execute('SELECT company_name, COUNT(vacancy_id) '
+                               'FROM companies '
+                               'JOIN vacancies USING (company_id) '
+                               'GROUP BY company_name;')
+
+                data = cursor.fetchall()
 
         except (Exception, psycopg2.DatabaseError) as error:
             return f'[INFO] {error}'
 
-        pass
+        connection.close()
+        return data
+
+    def get_all_vacancies(self):
+
+        try:
+            connection = psycopg2.connect(database=self.database_name, **self.params)
+            with connection.cursor() as cursor:
+                cursor.execute('SELECT title_vacancy, company_name, salary, vacancies.link '
+                               'FROM vacancies '
+                               'JOIN companies USING (company_id);')
+
+                data = cursor.fetchall()
+
+        except (Exception, psycopg2.DatabaseError) as error:
+            return f'[INFO] {error}'
+
+        connection.close()
+        return data
+
+    def get_avg_salary(self):
+
+        try:
+            connection = psycopg2.connect(database=self.database_name, **self.params)
+            with connection.cursor() as cursor:
+                cursor.execute('SELECT company_name, round(AVG(salary)) AS average_salary '
+                               'FROM companies '
+                               'JOIN vacancies USING (company_id) '
+                               'GROUP BY company_name;')
+
+                data = cursor.fetchall()
+
+        except (Exception, psycopg2.DatabaseError) as error:
+            return f'[INFO] {error}'
+
+        connection.close()
+        return data
